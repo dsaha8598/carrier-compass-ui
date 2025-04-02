1138d233337f7d665f33afcb7c355ec9dd0356c1
Dipak <dsaha8598@gmail.com>
2025-04-02 20:50:07 +0530

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { UserProfile } from './AfterLoggedInComponents/UserProfile';
 function App() {
   return (
     <React.StrictMode>
-      <BrowserRouter basename='/carrier-compass-ui'>
+      <BrowserRouter>
       <div className="flex">
         <SideBar></SideBar>
         <div className="flex-1 p-4">
